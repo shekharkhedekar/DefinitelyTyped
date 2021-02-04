b845a29f9794a04df18faf890ea8a3ad900c7f53
Shekhar Khedekar <shekharkhedekar@users.noreply.github.com>
2021-02-03 17:18:07 -0800

    export all interfaces

--- types/sitemap-generator/index.d.ts
+++ types/sitemap-generator/index.d.ts
@@ -5,12 +5,12 @@
 
 import Crawler = require('simplecrawler');
 
-type PriorityValues = 0.0 | 0.1 | 0.2 | 0.3 | 0.4 | 0.5 | 0.6 | 0.7 | 0.8 | 0.9 | 1.0;
-type FreqValues = 'always' | 'hourly' | 'daily' | 'weekly' | 'monthly' | 'yearly' | 'never';
-type EventTypes = 'add' | 'done' | 'error' | 'ignore';
-type ExcludeFunctionProps<T> = Pick<T, { [K in keyof T]: T[K] extends (...args: any[]) => void ? never : K }[keyof T]>;
+export type PriorityValues = 0.0 | 0.1 | 0.2 | 0.3 | 0.4 | 0.5 | 0.6 | 0.7 | 0.8 | 0.9 | 1.0;
+export type FreqValues = 'always' | 'hourly' | 'daily' | 'weekly' | 'monthly' | 'yearly' | 'never';
+export type EventTypes = 'add' | 'done' | 'error' | 'ignore';
+export type ExcludeFunctionProps<T> = Pick<T, { [K in keyof T]: T[K] extends (...args: any[]) => void ? never : K }[keyof T]>;
 
-type Options = Partial<ExcludeFunctionProps<Crawler>> & {
+export type Options = Partial<ExcludeFunctionProps<Crawler>> & {
     changeFreq?: FreqValues;
     filepath?: string | null;
     ignore?: (url: string) => boolean;
@@ -20,19 +20,19 @@ type Options = Partial<ExcludeFunctionProps<Crawler>> & {
     priorityMap?: PriorityValues[];
 };
 
-interface ErrorMessage {
+export interface ErrorMessage {
     code: string;
     message: string;
     url: string;
 }
 
-interface SiteMapRotator {
+export interface SiteMapRotator {
     getPaths: () => string[];
     addURL: (url: string, depth?: number, lastMod?: string) => void;
     finish: () => void;
 }
 
-interface Methods {
+export interface Methods {
     start: () => void;
     stop: () => void;
     getCrawler: () => Crawler;
